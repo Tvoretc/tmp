16e28935df7c26fff566ef1553ad604b5f8b5fc4
Tvoretc <rus.tvoretc@gmail.com>
2020-01-27 12:53:08 +0200

    1

---
+++ 85014782/Cluster_1/Master/modoverrides.lua
@@ -0,0 +1,22 @@
+return {
+  ["workshop-1185229307"]={ configuration_options={  }, enabled=true },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-458940297"]={
+    configuration_options={
+      DFV_ClientPrediction="default",
+      DFV_FueledSettings="default",
+      DFV_Language="EN",
+      DFV_MinimalMode="default",
+      DFV_PercentReplace="default",
+      DFV_ShowACondition="default",
+      DFV_ShowADefence="default",
+      DFV_ShowAType="default",
+      DFV_ShowDamage="default",
+      DFV_ShowFireTime="default",
+      DFV_ShowInsulation="default",
+      DFV_ShowTemperature="default",
+      DFV_ShowUses="default" 
+    },
+    enabled=true 
+  } 
+}
